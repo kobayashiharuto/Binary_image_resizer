0e1a2c9834c5606703e74488a613921a8327335d
kl <datesaver1234@gmail.com>
2021-06-01 03:55:57 +0900

    fix

--- main.py
+++ main.py
@@ -7,38 +7,6 @@ from PIL import Image
 import glob
 import matplotlib.pyplot as plt
 
-np.set_printoptions(threshold=10000)
-
-
-def show_image(image):
-    plt.imshow(image)
-    plt.show()
-
-
-def resize(path: str):
-    image = Image.open(path)
-    image_resized = image.resize(size=(50, 50))
-    image_gray = image.convert('L')
-    return image_gray
-
-
-def get_images_from_dir(path: str):
-    files = glob.glob(path + '/*')
-    return files
-
-
-def main():
-    tkinter.Tk().withdraw()
-    directory = tkinter.filedialog.askdirectory(
-        initialdir=os.path.abspath(os.path.dirname(__file__)))
-
-    paths = get_images_from_dir(directory)
-
-    for path in paths:
-        image = resize(path)
-        name = path.split('\\')[-1]
-        image.save('out/1/' + name)
-
 
 np.set_printoptions(threshold=10000)
 
@@ -81,9 +49,6 @@ def main():
         name = path.split('\\')[-1]
         image.save('out/1/' + name)
 
-
-if __name__ == '__main__':
-    main()
 
 if __name__ == '__main__':
     main()
